934c259c8351b86c7ac5dcf4c23745f628985bcf
Cesar Murillo <namiayumis@gmail.com>
2023-10-11 09:01:21 +0900

    add [database.type] package json

--- app/(dashboard)/_components/list.tsx
+++ app/(dashboard)/_components/list.tsx
@@ -2,9 +2,9 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
-
+import { type UsersEntity } from "@/types/collections";
 const List = () => {
-    const [users, setUsers] = useState<any[]>([])
+    const [users, setUsers] = useState<UsersEntity[]>([])
     const supabase = createClientComponentClient()
   
     useEffect(() => {
@@ -25,11 +25,11 @@ const List = () => {
         {users.map((user) => (
           <li key={user.email} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
-              <img
+              {/* <img
                 className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                src={user.imageUrl}
+                src={}
                 alt=""
-              />
+              /> */}
               <div className="min-w-0 flex-auto">
                 <p className="text-sm font-semibold leading-6 text-gray-900">
                   {user.name}
@@ -41,21 +41,13 @@ const List = () => {
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
               <p className="text-sm leading-6 text-gray-900">{user.role}</p>
-              {user.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen{" "}
-                  <time dateTime={user.lastSeenDateTime}>
-                    {user.name}
-                  </time>
-                </p>
-              ) : (
+
                 <div className="mt-1 flex items-center gap-x-1.5">
                   <div className="flex-none rounded-full bg-emerald-500/20 p-1">
                     <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
                   </div>
                   <p className="text-xs leading-5 text-gray-500">Online</p>
                 </div>
-              )}
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
               <p className="text-sm leading-6 text-gray-900">Vacaciones</p>
@@ -67,7 +59,7 @@ const List = () => {
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
               <p className="text-sm leading-6 text-gray-900">Compensatorios</p>
                 <div className="mt-1 flex items-center gap-x-1.5">
-                  <p className="text- leading-5 text-gray-500">{user.num_vacations}</p>
+                  <p className="text- leading-5 text-gray-500">{user.num_compensatorys}</p>
                 </div>
             </div>
           </li>
---
+++ types/collections.ts
@@ -0,0 +1,3 @@
+import { type Database } from "./database.type";
+
+export type UsersEntity = Database["public"]["Tables"]["users"]["Row"];
