6cf3bae6b9c9f2ad798521342207945a75bf35f9
Cesar Murillo <namiayumis@gmail.com>
2023-10-11 12:03:49 +0900

    [add] types by compensatorios

--- types/database.type.ts
+++ types/database.type.ts
@@ -9,6 +9,55 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      compensatorys: {
+        Row: {
+          approve_request: boolean | null
+          approved_by: string | null
+          approved_date: string | null
+          created_at: string
+          event_date: string | null
+          event_name: string | null
+          hours: number | null
+          id: string
+          user_id: string | null
+        }
+        Insert: {
+          approve_request?: boolean | null
+          approved_by?: string | null
+          approved_date?: string | null
+          created_at?: string
+          event_date?: string | null
+          event_name?: string | null
+          hours?: number | null
+          id?: string
+          user_id?: string | null
+        }
+        Update: {
+          approve_request?: boolean | null
+          approved_by?: string | null
+          approved_date?: string | null
+          created_at?: string
+          event_date?: string | null
+          event_name?: string | null
+          hours?: number | null
+          id?: string
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "compensatorys_approved_by_fkey"
+            columns: ["approved_by"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "compensatorys_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       users: {
         Row: {
           created_at: string
