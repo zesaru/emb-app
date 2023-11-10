7450132b201f7fb3e412eca4a150fbee8d82b7f1
Cesar Murillo <namiayumis@gmail.com>
2023-11-10 09:54:43 +0900

    Update user authentication logic This commit updates the user authentication logic to use a more secure hashing algorithm and adds error handling for invalid credentials.

---
+++ app/(dashboard)/(routes)/report/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import List from '../../_components/list'
+
+const Report = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="container mx-auto py-10">
+        <List/>
+    </div>
+  </div>)
+}
+
+export default Report
