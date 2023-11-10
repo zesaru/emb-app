865a58be1d73dfa7d04f316f73518a79dbc5fc07
Cesar Murillo <namiayumis@gmail.com>
2023-11-10 11:13:57 +0900

    Add authentication to report page

--- app/(dashboard)/(routes)/report/page.tsx
+++ app/(dashboard)/(routes)/report/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+export const dynamic = "force-dynamic";
 import List from '../../_components/list'
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-const Report = () => {
+const Report = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session === null) {
+    redirect("/login");
+  }
+    
   return (
     <div className="flex flex-col">
       <div className="container mx-auto py-10">
